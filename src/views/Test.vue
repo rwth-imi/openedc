acb16a575f5b03a016c8392a83228c3ae22f7c11
Rabea de Groot <rabea.degroot@rwth-aachen.de>
2020-09-15 18:14:35 +0200

    first test input fields

---
+++ src/views/Test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="">
+      <file-upload-field></file-upload-field>
+    </div>
+</template>
+
+<script>
+    import FileUploadField from '@/components/input_fields/FileUploadField.vue'
+    export default {
+        name: "Test",
+        components: {
+          FileUploadField
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

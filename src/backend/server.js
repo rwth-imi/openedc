a1ea33963e4de73801dffa992852f643ecf57771
Rabea de Groot <rabea.degroot@rwth-aachen.de>
2020-10-08 11:42:07 +0200

    request to server work

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+
+  next()
+})
+
 app.get("/", function(req, res) {
   res.send("Hello World!");
 });
+
+app.post("/api/upload/", (req, res, next) => {
+
+  res.json({ 'test': 'success'})
+});
+app.get("/api/upload/", function(req, res) {
+  res.send("Hello Upload!");
+})
 
 app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
--- src/views/CreateCRF.vue
+++ src/views/CreateCRF.vue
@@ -1,10 +1,17 @@
 <template>
-<div class="mainPage">
-  <h1>Create CRF</h1>
-  <div>
-    <b-card bg-variant="light">
-      <b-form-group label-cols-lg="3" label="Create CRF" label-size="lg" label-class="font-weight-bold pt-0" class="mb-0">
-        <!-- <b-form-group label-cols-sm="3" label="Name:" label-align-sm="right" label-for="nested-name">
+  <div class="mainPage">
+    <h1>Create CRF</h1>
+    <div>
+      <b-card bg-variant="light">
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+          <b-form-group
+            label-cols-lg="3"
+            label="Create CRF"
+            label-size="lg"
+            label-class="font-weight-bold pt-0"
+            class="mb-0"
+          >
+            <!-- <b-form-group label-cols-sm="3" label="Name:" label-align-sm="right" label-for="nested-name">
           <b-form-input id="nested-name"></b-form-input>
         </b-form-group>
 
@@ -24,33 +31,61 @@
           <b-form-input id="nested-version-description"></b-form-input>
         </b-form-group> -->
 
-        <b-form-group label-cols-sm="3" label="Upload File:" label-align-sm="right" label-for="nested-file">
-          <b-form-file
-            v-model="file1"
-            :state="Boolean(file1)"
-            placeholder="Choose a file or drop it here..."
-            drop-placeholder="Drop file here..."
-          ></b-form-file>
-        </b-form-group>
-      </b-form-group>
-    </b-card>
+            <b-form-group
+              label-cols-sm="3"
+              label="Upload File:"
+              label-align-sm="right"
+              label-for="nested-file"
+            >
+              <b-form-file
+                v-model="form.file"
+                :state="Boolean(form.file)"
+                placeholder="Choose a file or drop it here..."
+                drop-placeholder="Drop file here..."
+              ></b-form-file>
+            </b-form-group>
+          </b-form-group>
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-form>
+      </b-card>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
   name: "CRFs",
-  components: {
-  },
+  components: {},
   data() {
     return {
-      file1: [],
+      form: {
+        file: []
+      },
+      show: true
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      console.log(this.form)
+      this.axios.post('upload/', this.form)
+        .then((data) => {
+          console.log('response', data)
+        })
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.file = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
     }
   }
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

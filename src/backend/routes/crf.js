6f85f4838d18c7b898149820ed821075a7120a5d
Rabea de Groot <rabea.degroot@rwth-aachen.de>
2020-10-30 15:36:31 +0100

    server creates uploads directory

--- src/backend/routes/crf.js
+++ src/backend/routes/crf.js
@@ -5,10 +5,16 @@ const fs = require("fs");
 const path = require("path");
 const db = require('./stores.js');
 const utils = require("./utils.js")
+
+const uploadDir = "uploads/";
+
+if(!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 // File Upload
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, uploadDir)
   },
   filename: function(req, file, cb) {
     cb(null, req.body.filename)

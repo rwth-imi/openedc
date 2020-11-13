c257f055388a8efe4c9d28e7f73feadc6821157f
Rabea de Groot <rabea.degroot@rwth-aachen.de>
2020-11-13 11:54:34 +0100

    moved i18n

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import i18n from "./i18n";
+import i18n from "./plugins/i18n";
 
 
 Vue.config.productionTip = false;
